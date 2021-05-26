cb239eb80f27839c98571907177e420be01e0719
d-murphy <murphy.dan.s@gmail.com>
2021-05-26 13:59:50 -0400

    corrected typo in pybroker_erorrs.py explanation

--- pylib/pybroker/pybroker/pybroker_errors.py
+++ pylib/pybroker/pybroker/pybroker_errors.py
@@ -3,7 +3,7 @@ class BrokerError(Exception):
 
 class MissingReplyFieldError(BrokerError):
     """Raised when a client requests an event replay but neglects to supply
-    a expected elements such as the reply_to address or correlation_id."""
+    the expected elements such as the reply_to address or correlation_id."""
     def __init__(self):
         self.msg = "Request is missing expected elements, possibly the \
                     reply_to or correlation_id fields. \
