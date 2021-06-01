69c8dcbc40180c9e0afb15055af0866b92de9c13
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2021-06-01 09:17:18 -0400

    WriteModel: ccleaning up trivial imports

--- writemodel/app/db_client.py
+++ writemodel/app/db_client.py
@@ -1,19 +1,17 @@
 """Provides easy shell access to the database"""
 
-from sqlalchemy import create_engine, select
+from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from write_model import WriteModel
 from state_manager.schema import CitationHash
-
-enjoy = 'wait, what?'
 
 if __name__ == '__main__':
     wm = WriteModel()
     db = wm.manager.db
     engine = db._engine
     print('.' * 79)
-    print('\nWelcome to the EventStore interactive client.\n')
+    print('\nWelcome to the Write Model interactive client.\n')
     print('The following objects are available in the local namespace:')
     for obj in ('wm: WriteModel()', 'db: Database()', 'engine: Database._engine()',
                 'CitationHash',
