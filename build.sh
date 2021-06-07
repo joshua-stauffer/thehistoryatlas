7191478fcca6032a9290ca466c3c3a21a6d50a3b
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2021-06-06 21:54:53 -0400

    Build script now generates dmock data, and creates virtual environment, closes #1

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# This shell script builds the local docker images needed by the application.
-
+# This shell script builds the local docker images needed by the application,
+# and installs a virtual environment for running scripts and tests.
+python3 -m venv test_env
+source test_env/bin/activate
+pip install --upgrade pip requests sqlalchemy pytest pytest-asyncio
 docker build -t pylib ./pylib
 docker build -t testlib ./testlib
 docker-compose build
@@ -12,6 +15,10 @@ echo ""
 echo "To start the project, run:"
 echo "    docker-compose up"
 echo "in the root directory."
+echo ""
+echo "After the project is running, fill the database with either real or"
+echo "generated data by running:"
+echo "    source test_env/bin/activate && python builder/scripts/mockclient.py"
 echo ""
 echo "The RabbitMQ admin console is available at:"
 echo "    http://localhost:15672"
