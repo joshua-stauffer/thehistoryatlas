66ad240d560fbff6e59f5fcc3a32cf57cbe92898
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2022-08-14 13:54:08 -0400

    tests back to baseline

--- nlp/tests/test_database.py
+++ nlp/tests/test_database.py
@@ -8,7 +8,7 @@ from sqlalchemy import select
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Session
 from nlp_service.state.database import Database
-from nlp_service.state.schema import AnnotatedCitation
+from nlp_service.state.schema import AnnotatedCitation, Base
 from nlp_service.state.schema import Entity
 
 log = getLogger(__name__)
@@ -22,17 +22,13 @@ class Config:
     def __init__(self):
         self.DB_URI = "postgresql+psycopg2://postgres:hardpass123@localhost:5432/nlp"
         self.DEBUG = False  # outputs all activity
-        # TODO: make this dynamic
-        self.TRAIN_DIR = (
-            "/Users/josh/dev/thehistoryatlas/nlp/tests/test_train_dir/train.json"
-        )
 
 
-def fill_db(config: Config, engine: Engine):
+def fill_db(engine: Engine, training_dir: str):
     """Loads database with files found in base_training_data"""
     log.info("Filling the DB with initial training data")
     training_data = list()
-    for file in os.scandir(config.TRAIN_DIR):
+    for file in os.scandir(training_dir):
         if os.path.isfile(file) and file.name.endswith(".json"):
             with open(file.path, "r") as f:
                 json_file = json.load(f)
@@ -57,11 +53,16 @@ def fill_db(config: Config, engine: Engine):
         ]
         to_commit.extend(entity_list)
 
-    to_commit.append(init)
     log.info(f"Initializing DB with {len(to_commit)} objects")
-    with Session(self._engine, future=True) as session:
+    with Session(engine, future=True) as session:
         session.add_all(to_commit)
         session.commit()
+
+
+def reset_db(db):
+    Base.metadata.drop_all(db._engine)
+    Base.metadata.create_all(db._engine)
+    fill_db(db._engine, db._config.TRAIN_DIR)
 
 
 @pytest.fixture
@@ -72,23 +73,17 @@ def config():
 @pytest.fixture
 def db(config):
     root = os.getcwd()
-    config.TRAIN_DIR = root + "/tests/test_train_dir"
+    if root.endswith("nlp"):
+        config.TRAIN_DIR = root + "/tests/test_train_dir"
+    elif root.endswith("tests"):
+        config.TRAIN_DIR = root + "/test_train_dir"
     config.debug = True
     db = Database(config)
-
-    return db
-
-
-def test_db_exists(db):
-    assert db != None
-
-
-def test_is_db_empty(db):
-    assert db._db_is_empty() == False
+    reset_db(db)
+    yield db
 
 
 def test_db_content(db):
-    db._fill_db()
     with Session(db._engine, future=True) as session:
         annotated_citations = session.execute(select(AnnotatedCitation)).scalars()
         # there are results, right?
