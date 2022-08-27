02902569cefa3f305f1d722bd029b753e56ae987
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2022-08-27 19:59:07 -0400

    cleanup event schema

--- pylib/event-schema/event_schema/EventSchema.py
+++ pylib/event-schema/event_schema/EventSchema.py
@@ -3,7 +3,7 @@ SQLAlchemy database schema for the primary store of events in the History Atlas.
 Used by both the History service and the EventStore.
 """
 
-from sqlalchemy import INTEGER, Column
+from sqlalchemy import Column
 from sqlalchemy.dialects.postgresql import VARCHAR, INTEGER, JSONB
 from sqlalchemy.orm import declarative_base
 
@@ -14,16 +14,15 @@ class Event(Base):
     __tablename__ = "events"
 
     index = Column(INTEGER, primary_key=True)
-    type = Column(VARCHAR, nullable=False)  # string
+    type = Column(VARCHAR, nullable=False)
     transaction_id = Column(
         VARCHAR, nullable=False
-    )  # group atomic events together if necessary
+    )
     app_version = Column(
         VARCHAR, nullable=False
-    )  # keep track of which version created this event
-    # to help with future compatability
+    )
     timestamp = Column(VARCHAR, nullable=False)  # Www, dd Mmm yyyy hh:mm:ss GMT
-    user_id = Column(VARCHAR, nullable=False)  # user guid
+    user_id = Column(VARCHAR, nullable=False)
     payload = Column(JSONB, nullable=False)
 
     def __repr__(self):
