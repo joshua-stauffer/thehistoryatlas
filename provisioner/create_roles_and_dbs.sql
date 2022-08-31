eccac6fb4e22a042132aef87f7c8ebae0ae9af02
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2022-08-31 15:07:59 +0200

    update provisioner to use env vars

---
+++ provisioner/create_roles_and_dbs.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE eventstore;
+CREATE ROLE readwrite_eventstore;
+
+CREATE DATABASE readmodel;
+CREATE ROLE readwrite_readmodel;
+
+CREATE DATABASE writemodel;
+CREATE ROLE readwrite_writemodel;
+
+CREATE DATABASE accounts;
+CREATE ROLE readwrite_accounts;
+
+CREATE DATABASE geo;
+CREATE ROLE readwrite_geo;
+
+CREATE DATABASE nlp;
+CREATE ROLE readwrite_nlp;
