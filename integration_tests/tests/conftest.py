ec39788ae31853e761adaf796a49cafbabe315b4
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2022-09-06 10:02:04 +0200

    lint

--- integration_tests/tests/conftest.py
+++ integration_tests/tests/conftest.py
@@ -7,7 +7,6 @@ from tha_config import Config
 @pytest.fixture(scope="session")
 def TestBroker():
     class TestBroker(BrokerBase):
-
         def __init__(self, queue_name: str, listen_to: str):
             self._listen_to = listen_to
             config = Config()
@@ -17,24 +16,23 @@ def TestBroker():
                 broker_password=config.BROKER_PASS,
                 network_host_name="localhost",  # "=config.NETWORK_HOST_NAME,
                 exchange_name="main",
-                queue_name=queue_name
+                queue_name=queue_name,
             )
 
         async def start(self):
             await self.connect()
             await self.add_message_handler(
-                routing_key=self._listen_to,
-                callback=self.handle_message
+                routing_key=self._listen_to, callback=self.handle_message
             )
 
         async def handle_message(self, message):
             body = self.decode_message(message)
             self.results.append(body)
+
     return TestBroker
 
 
 class TestBroker(BrokerBase):
-
     def __init__(self, queue_name: str, listen_to: str):
         self._listen_to = listen_to
         config = Config()
@@ -44,14 +42,13 @@ class TestBroker(BrokerBase):
             broker_password=config.BROKER_PASS,
             network_host_name="localhost",  # "=config.NETWORK_HOST_NAME,
             exchange_name="main",
-            queue_name=queue_name
+            queue_name=queue_name,
         )
 
     async def start(self):
         await self.connect()
         await self.add_message_handler(
-            routing_key=self._listen_to,
-            callback=self.handle_message
+            routing_key=self._listen_to, callback=self.handle_message
         )
 
     async def handle_message(self, message):
--- integration_tests/tests/test_write_model/test_write_model.py
+++ integration_tests/tests/test_write_model/test_write_model.py
@@ -15,7 +15,6 @@ PUBLISH_NEW_CITATION_COMMAND = ADD_NEW_CITATION_API_OUTPUT[0]
 
 
 class TestBroker(BrokerBase):
-
     def __init__(self, queue_name: str, listen_to: str):
         self._listen_to = listen_to
         config = Config()
@@ -25,14 +24,13 @@ class TestBroker(BrokerBase):
             broker_password=config.BROKER_PASS,
             network_host_name="localhost",  # "=config.NETWORK_HOST_NAME,
             exchange_name="main",
-            queue_name=queue_name
+            queue_name=queue_name,
         )
 
     async def start(self):
         await self.connect()
         await self.add_message_handler(
-            routing_key=self._listen_to,
-            callback=self.handle_message
+            routing_key=self._listen_to, callback=self.handle_message
         )
 
     async def handle_message(self, message):
@@ -40,15 +38,11 @@ class TestBroker(BrokerBase):
         self.results.append(body)
 
 
-
 @pytest.fixture(scope="session")
 def publish_new_citation_broker():
     """Instantiate a broker to handle the PUBLISH_NEW_CITATION event."""
     # results will be available via broker.results
-    broker = TestBroker(
-        queue_name="test_broker",
-        listen_to="event.emitted"
-    )
+    broker = TestBroker(queue_name="test_broker", listen_to="event.emitted")
     return broker
 
 
@@ -81,7 +75,9 @@ async def setup_add_new_citation_events(publish_new_citation_broker):
 
 @pytest.fixture(scope="session")
 def add_new_citation_events(publish_new_citation_broker):
-    events = [from_dict(event_dict) for event_dict in publish_new_citation_broker.results[0]]
+    events = [
+        from_dict(event_dict) for event_dict in publish_new_citation_broker.results[0]
+    ]
     return events
 
 
@@ -103,7 +99,6 @@ def person_added(add_new_citation_events):
 @pytest.fixture(scope="session")
 def place_added(add_new_citation_events):
     return add_new_citation_events[3]
-
 
 
 @pytest.fixture(scope="session")
@@ -131,8 +126,8 @@ async def test_run_test(setup_add_new_citation_events):
         (2, "PERSON_ADDED"),
         (3, "PLACE_ADDED"),
         (4, "TIME_ADDED"),
-        (5, "META_ADDED")
-    ]
+        (5, "META_ADDED"),
+    ],
 )
 def test_add_new_citation_event_types(index, value, add_new_citation_events):
     assert getattr(add_new_citation_events[index], "type") == value
@@ -160,10 +155,12 @@ def test_add_new_citation_transaction_ids_are_uuids(add_new_citation_events):
         ("user_id", PUBLISH_NEW_CITATION_COMMAND["user"]),
         ("app_version", PUBLISH_NEW_CITATION_COMMAND["app_version"]),
         ("timestamp", PUBLISH_NEW_CITATION_COMMAND["timestamp"]),
-        ("index", None)  # index will only be populated after event is persisted
-    ]
+        ("index", None),  # index will only be populated after event is persisted
+    ],
 )
-def test_add_new_citation_events_return_same_top_level_attrs(attr, value, add_new_citation_events):
+def test_add_new_citation_events_return_same_top_level_attrs(
+    attr, value, add_new_citation_events
+):
     for event in add_new_citation_events:
         assert getattr(event, attr) == value
 
@@ -186,20 +183,21 @@ def test_add_new_citation_events_return_same_citation_id(add_new_citation_events
 
 
 def test_citation_added_text(citation_added):
-    assert citation_added.payload.text == PUBLISH_NEW_CITATION_COMMAND["payload"]["text"]
+    assert (
+        citation_added.payload.text == PUBLISH_NEW_CITATION_COMMAND["payload"]["text"]
+    )
 
 
 def test_summary_added_text(summary_added):
-    assert summary_added.payload.text == PUBLISH_NEW_CITATION_COMMAND["payload"]["summary"]["text"]
+    assert (
+        summary_added.payload.text
+        == PUBLISH_NEW_CITATION_COMMAND["payload"]["summary"]["text"]
+    )
 
 
 @pytest.mark.parametrize(
     "adm_attr,api_attr",
-    [
-        ("name", "name"),
-        ("citation_start", "start_char"),
-        ("citation_end", "stop_char")
-    ]
+    [("name", "name"), ("citation_start", "start_char"), ("citation_end", "stop_char")],
 )
 def test_add_new_person(adm_attr, api_attr, person_added):
     person_added = add_new_citation_events[2]
@@ -215,8 +213,8 @@ def test_add_new_person(adm_attr, api_attr, person_added):
         ("citation_end", "stop_char"),
         ("latitude", "latitude"),
         ("longitude", "longitude"),
-        ("geo_shape", "geoshape")
-    ]
+        ("geo_shape", "geoshape"),
+    ],
 )
 def test_add_new_place(adm_attr, api_attr, place_added):
     tag = PUBLISH_NEW_CITATION_COMMAND["payload"]["tags"][1]
@@ -225,25 +223,19 @@ def test_add_new_place(adm_attr, api_attr, place_added):
 
 @pytest.mark.parametrize(
     "adm_attr,api_attr",
-    [
-        ("name", "name"),
-        ("citation_start", "start_char"),
-        ("citation_end", "stop_char")
-    ]
+    [("name", "name"), ("citation_start", "start_char"), ("citation_end", "stop_char")],
 )
 def test_add_new_time(adm_attr, api_attr, time_added):
     tag = PUBLISH_NEW_CITATION_COMMAND["payload"]["tags"][2]
     assert getattr(time_added.payload, adm_attr) == tag[api_attr]
 
 
-@pytest.mark.parametrize(
-    "attr",
-    [
-        "title", "author", "publisher"
-    ]
-)
+@pytest.mark.parametrize("attr", ["title", "author", "publisher"])
 def test_add_meta_fields(attr, meta_added):
-    assert getattr(meta_added.payload, attr) == PUBLISH_NEW_CITATION_COMMAND["payload"]["meta"][attr]
+    assert (
+        getattr(meta_added.payload, attr)
+        == PUBLISH_NEW_CITATION_COMMAND["payload"]["meta"][attr]
+    )
 
 
 def test_add_meta_kwargs(meta_added):
@@ -260,4 +252,3 @@ async def test_cleanup(publish_new_citation_broker):
         await publish_new_citation_broker.cancel()
     except RuntimeError:
         pass
-
