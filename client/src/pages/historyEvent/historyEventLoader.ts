b22126f5bbd7c73f98eb05f3aa184924c85a3960
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-12-29 12:32:08 +0100

    remove unused code to unblock deploy

--- client/src/pages/historyEvent/historyEventLoader.ts
+++ client/src/pages/historyEvent/historyEventLoader.ts
@@ -32,61 +32,6 @@ export interface HistoryEventData {
   loadPrev: () => Promise<HistoryEvent[]>;
 }
 
-const eventMap = new Map<string, HistoryEvent>([]);
-events.map((historyEvent) => {
-  eventMap.set(historyEvent.id, historyEvent);
-});
-
-const storyMap = new Map<string, Story>([]);
-stories.map((story) => {
-  storyMap.set(story.id, story);
-});
-
-export const historyEventLoader = ({
-  params: { eventId, storyId },
-}: LoaderFunctionArgs): HistoryEventData => {
-  if (eventId === undefined || storyId === undefined) {
-    throw new Error("eventId and storyId are required parameters.");
-  }
-  const story = storyMap.get(storyId);
-  if (story === undefined) {
-    throw new Error("Story not found");
-  }
-  const currentEvent = eventMap.get(eventId);
-  if (currentEvent === undefined) {
-    throw new Error("Event not found");
-  }
-  const eventIndex = story.events.findIndex(
-    (historyEvent) => historyEvent.id === currentEvent.id
-  );
-  const nextEvent: HistoryEvent | null = story.events[eventIndex + 1] || null;
-  let index = 1;
-  let prevEvent: HistoryEvent | null;
-  if (eventIndex === 0) {
-    // no previous event
-    index = 0;
-    prevEvent = null;
-  } else {
-    prevEvent = story.events[eventIndex - 1] || null;
-  }
-  const maybeEvents = [prevEvent, currentEvent, nextEvent];
-  const events: HistoryEvent[] = [];
-  for (const event of maybeEvents) {
-    if (event) {
-      events.push(event);
-    }
-  }
-  const dummyLoader = () => [];
-  return {
-    events,
-    story,
-    index,
-    currentEvent,
-    loadPrev: dummyLoader,
-    loadNext: dummyLoader,
-  };
-};
-
 faker.seed(872);
 
 export const fakeHistoryEventLoader = ({
