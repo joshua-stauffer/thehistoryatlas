3e53551ac31bf6a3c1bf442c1b71554c310f49ed
Joshua Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2024-12-30 11:55:00 +0100

    serve fake data

--- server/the_history_atlas/api/main.py
+++ server/the_history_atlas/api/main.py
@@ -5,11 +5,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from the_history_atlas.api.context import get_context
+from the_history_atlas.api.rest import register_rest_endpoints
 from the_history_atlas.api.schema import build_schema
 from the_history_atlas.apps.app_manager import AppManager
 
 
 def mount_api(app: FastAPI, app_manager: AppManager) -> FastAPI:
+    app = register_rest_endpoints(app)
     schema = build_schema()
     get_context_value = partial(get_context, _apps=app_manager)
     graphql = GraphQL(schema, debug=True, context_value=get_context_value)
