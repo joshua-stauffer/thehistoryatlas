9c150e167cf1a2c7ead21cb5f025418a24cbc8d3
Josh Stauffer <66793731+joshua-stauffer@users.noreply.github.com>
2025-03-22 08:34:27 +0100

    update default

--- wiki_link/wiki_service/config.py
+++ wiki_link/wiki_service/config.py
@@ -5,7 +5,7 @@ class WikiServiceConfig:
     def __init__(self):
         self.DB_URI = os.environ.get("THA_DB_URI")
         self.DEBUG = bool(os.environ.get("DEBUG"))
-        self.WIKIDATA_SEARCH_LIMIT = 10
+        self.WIKIDATA_SEARCH_LIMIT = 1000
         self.WIKIDATA_SPARQL_URL = "https://query.wikidata.org/sparql"
         self.USER_ID = "a5749422-65c1-4ce5-a582-7b08c3e71de6"
         self.username = os.environ.get("WIKILINK_USERNAME")
